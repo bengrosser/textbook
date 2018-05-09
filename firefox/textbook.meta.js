c05bedf71303d3c28271b327bcee7208bff851e3
Ben Grosser <grosser@bengrosser.com>
2018-05-09 00:59:15 -0500

    1.0.3 release

--- firefox/textbook.meta.js
+++ firefox/textbook.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Textbook
-// @version 1.0.2
+// @version 1.0.3
 // @namespace com.bengrosser.textbook
 // @description Removes images from Facebook, leaving only text
 // @updateURL http://bengrosser.com/share/tb/textbook.meta.js
